0d0e20492ac57f76dde0b6fcfa305bbc078c1e60
Ivo_Točený <xtocen00@vutbr.cz>
2021-04-06 20:09:52 +0200

    CV8 start

--- Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/xsim_webtalk.tcl
+++ Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 30 22:10:19 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Mar 30 23:32:47 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "711ced0b-7ca1-4d25-b79e-10b252b7bd66" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "46168cc0ffde4713aa4427dfe2d31b9c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6400_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2234196192 -regid "" -xml D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6388_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 584139278 -regid "" -xml D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Documents/Toceny_222683/Digital-electronics-1/Labs/07-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_t_ff_rst_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
---
+++ Labs/08-traffic_lights/README.md
@@ -0,0 +1,18 @@
+# Ivo Točený, 222683 - 08-traffic_lights
+
+## 0. part - Link to GitHub repository
+
+[My GitHub 08-traffic_lights repository](https://github.com/Ivo-Toceny-222683/Digital-electronics-1/tree/main/Labs/08-traffic_lights)
+
+## 1. part - Preparation tasks
+
+### State table:
+
+| **Input P** | `0` | `0` | `1` | `1` | `0` | `1` | `0` | `1` | `1` | `1` | `1` | `0` | `0` | `1` | `1` | `1` |
+| :-- | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
+| **Clock** | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) | ![rising](Images/eq_uparrow.png) |
+| **State** | A | A | B | C | C | D | A | B |  |  |  |  |  |  |  |  |
+| **Output R** | `0` | `0` | `0` | `0` | `0` | `1` | `0` | `0` | `0` | `0` | `0` | `0` | `0` | `0` | `0` | `0` |
+
+### Figure with connection of RGB LEDs on Nexys A7 board and completed table with color settings
+
